7b8c6b4fa2439b699a502459f9822323ffb2493e
Emir <emircengiz21@gmail.com>
2021-12-21 13:12:56 +0300

    dxgfgyhuj

---
+++ Math/Vector2.py
@@ -0,0 +1,14 @@
+
+
+class Vector2(object):
+    def __init__(self, x, y):
+        super(Vector2, self).__init__(x, y)
+        self.x = x
+        self.y = y
+
+    def get_position(self):
+        return [self.x, self.y]
+
+    def set_position(self, x, y):
+        self.x = x
+        self.y = y
--- components/Player.py
+++ components/Player.py
@@ -1,11 +1,14 @@
 
 from components.Component import Component
-
+from Math.Vector2 import Vector2
 
 class Player(object):
     def __init__(self, *args):
         super(Player, self).__init__(*args)
         self.components = []
+        self.texture = None
+        self.position : Vector2 = Vector2(None, None)
+
 
     def attach_component(self, component: Component):
         self.components.insert(component)
